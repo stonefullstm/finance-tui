d3d05bd28d19be5b0fb5c6c493d99f9fd2a9596e
stonefullstm <stonefull.stm@gmail.com>
2026-02-15 18:14:51 -0300

    feat: Create CSS and exit() function

--- finance/tui.py
+++ finance/tui.py
@@ -4,6 +4,29 @@ from textual.screen import Screen
 from textual.widgets import Button, DataTable, Footer, Header, Input, Label, Static
 
 
+class QuestionDialog(Screen):
+    def __init__(self, message, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.message = message
+
+    def compose(self):
+        no_button = Button("No", variant="primary", id="no")
+        no_button.focus()
+
+        yield Grid(
+            Label(self.message, id="question"),
+            Button("Yes", variant="error", id="yes"),
+            no_button,
+            id="question-dialog",
+        )
+
+    def on_button_pressed(self, event):
+        if event.button.id == "yes":
+            self.dismiss(True)
+        else:
+            self.dismiss(False)
+
+
 class FinanceApp(App):
     CSS_PATH = "finance.tcss"
     BINDINGS = [
@@ -45,6 +68,13 @@ class FinanceApp(App):
         self.title = "Personal Finance Manager"
         self.sub_title = "A Finance Manager App With Textual & Python"
         self.load_transactions()
+
+    def action_request_quit(self):
+        def check_answer(accepted):
+            if accepted:
+                self.exit()
+
+        self.push_screen(QuestionDialog("Do you want to quit?"), check_answer)
 
     def load_transactions(self):
         transactions_list = self.query_one(".transactions-list", DataTable)
