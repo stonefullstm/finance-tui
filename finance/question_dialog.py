486c59b59b2fc1cc1e5b0148e30a7038ed8f3fa0
stonefullstm <stonefull.stm@gmail.com>
2026-02-16 08:41:50 -0300

    refactor: Create question_dialog.py file to QuestionDialog class

---
+++ finance/question_dialog.py
@@ -0,0 +1,26 @@
+from textual.screen import Screen
+from textual.widgets import Button, Label
+from textual.containers import Grid
+
+
+class QuestionDialog(Screen):
+    def __init__(self, message, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.message = message
+
+    def compose(self):
+        no_button = Button("No", variant="primary", id="no")
+        no_button.focus()
+
+        yield Grid(
+            Label(self.message, id="question"),
+            Button("Yes", variant="error", id="yes"),
+            no_button,
+            id="question-dialog",
+        )
+
+    def on_button_pressed(self, event):
+        if event.button.id == "yes":
+            self.dismiss(True)
+        else:
+            self.dismiss(False)
--- finance/tui.py
+++ finance/tui.py
@@ -13,29 +13,30 @@ from textual.widgets import (
     Select,
 )
 from dao.transaction_dao import TransactionDAO
+from finance.question_dialog import QuestionDialog
 
 
-class QuestionDialog(Screen):
-    def __init__(self, message, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.message = message
+# class QuestionDialog(Screen):
+#     def __init__(self, message, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.message = message
 
-    def compose(self):
-        no_button = Button("No", variant="primary", id="no")
-        no_button.focus()
+#     def compose(self):
+#         no_button = Button("No", variant="primary", id="no")
+#         no_button.focus()
 
-        yield Grid(
-            Label(self.message, id="question"),
-            Button("Yes", variant="error", id="yes"),
-            no_button,
-            id="question-dialog",
-        )
+#         yield Grid(
+#             Label(self.message, id="question"),
+#             Button("Yes", variant="error", id="yes"),
+#             no_button,
+#             id="question-dialog",
+#         )
 
-    def on_button_pressed(self, event):
-        if event.button.id == "yes":
-            self.dismiss(True)
-        else:
-            self.dismiss(False)
+#     def on_button_pressed(self, event):
+#         if event.button.id == "yes":
+#             self.dismiss(True)
+#         else:
+#             self.dismiss(False)
 
 
 class InputDialog(Screen):
